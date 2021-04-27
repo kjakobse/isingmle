170ed15505346e5f0b30d97232f17ef1abdf0b75
Ebbe Jakobsen <ebbe.jakobsen@keylane.com>
2021-04-27 22:30:12 +0200

    First (failed) attempt a getting parallel running to work with library.

---
+++ ising/R/ising.R
@@ -0,0 +1,12 @@
+#' ising: A package for computing the xxx
+#'
+#' The ising package provides three categories of important functions:
+#' x, y and z.
+#'
+#' @section ising functions:
+#' The ising functions ...
+#'
+#' @docType package
+#' @name ising
+NULL
+#> NULL
