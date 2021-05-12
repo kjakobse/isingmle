28225afac7c6250b8483a4070a56deb67a1f6925
Kim Daniel Jakobsen <kim.jakobsen@jc-aps.dk>
2021-05-12 20:22:40 +0200

    Updated formatting

--- ising/R/ising.R
+++ ising/R/ising.R
@@ -1,14 +1,14 @@
-#' ising: A package for computing maximum likelihood estimator in a graphical Ising model with MTP_2 constraints
+#' ising: compute maximum likelihood estimators in graphical Ising models
 #'
 #' The ising package provides functions for maximum likelihood estimation, sampling, computing likelihoods, and performing likelihood ratio tests with bootstrapping in graphical Ising models with MTP_2 constraints.
 #'
 #'
 #' @section ising functions:
-#' isingMLE: Find the maximum likelihood estimate in in a graphical Ising model.\cr
-#' IsingMLEmtp2: Find the maximum likelihood estimate in a graphical Ising model with MTP_2 constraints. \cr
-#' IsingSampler: Sample from a graphical Ising model. \cr
-#' IsingLikelihood: Compute the likelihood of a dataset from a graphical Ising model.\cr
-#' BootstrapLikelihoodRatio: Simulate from the null-distribution to estimate the distribution of the likelihood ratio test statistic.
+#' \strong{isingMLE}: Find the maximum likelihood estimate in in a graphical Ising model.\cr
+#' \strong{IsingMLEmtp2}: Find the maximum likelihood estimate in a graphical Ising model with MTP_2 constraints. \cr
+#' \strong{IsingSampler}: Sample from a graphical Ising model. \cr
+#' \strong{IsingLikelihood}: Compute the likelihood of a dataset from a graphical Ising model. \cr
+#' \strong{BootstrapLikelihoodRatio}: Simulate from the null-distribution to estimate the distribution of the likelihood ratio test statistic. \cr
 #'
 #'
 #' @docType package
