c6b5bee429b019280cab9d07ce8e41bb34398fd9
Kim Daniel Jakobsen <kim.jakobsen@jc-aps.dk>
2021-05-12 20:02:56 +0200

    Added package description.

--- ising/R/ising.R
+++ ising/R/ising.R
@@ -1,10 +1,15 @@
-#' ising: A package for computing the xxx
+#' ising: A package for computing maximum likelihood estimator in a graphical Ising model with MTP_2 constraints
 #'
-#' The ising package provides three categories of important functions:
-#' x, y and z.
+#' The ising package provides functions for maximum likelihood estimation, sampling, computing likelihoods, and performing likelihood ratio tests with bootstrapping in graphical Ising models with MTP_2 constraints.
+#'
 #'
 #' @section ising functions:
-#' The ising functions ...
+#' isingMLE: Find the maximum likelihood estimate in in a graphical Ising model.\cr
+#' IsingMLEmtp2: Find the maximum likelihood estimate in a graphical Ising model with MTP_2 constraints. \cr
+#' IsingSampler: Sample from a graphical Ising model. \cr
+#' IsingLikelihood: Compute the likelihood of a dataset from a graphical Ising model.\cr
+#' BootstrapLikelihoodRatio: Simulate from the null-distribution to estimate the distribution of the likelihood ratio test statistic.
+#'
 #'
 #' @docType package
 #' @name ising
